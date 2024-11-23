5030bc1990b402f0254db8f3f3044bd88c0a0ee5
fathyafathazz <fathyafathazzra@gmail.com>
2024-11-24 01:19:38 +0700

    ulang

--- qr.js
+++ qr.js
@@ -6,8 +6,10 @@ import {getJSON} from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import {redirect} from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
 if (getCookie("login") === "") {
-    setInner("content", "Silakan login untuk melanjutkan."); 
+    // Tetap di halaman login
+    setInner("content", "Silakan login untuk melanjutkan."); // Menampilkan pesan untuk login
 } else {
+    // Ambil data user
     getJSON("https://asia-southeast2-awangga.cloudfunctions.net/logiccoffee/data/user", "login", getCookie("login"), validateRole);
 }
 
@@ -16,24 +18,29 @@ function validateRole(result) {
         setInner("content", "Silahkan lakukan pendaftaran terlebih dahulu " + result.data.name);
         redirect("/register");
     } else {
+        // Cek role untuk mengarahkan halaman
         const userRole = result.data.role;
 
-        if (userRole === "admin") {
-            wauthparam.redirect = "/admin/dashboard";
+        // Role yang diizinkan untuk mengakses halaman home
+        const allowedRolesForHome = ["user", "dosen"];
+
+        if (allowedRolesForHome.includes(userRole)) {
+            redirect("/menu"); // Arahkan user atau dosen ke halaman home
+        } else if (userRole === "admin") {
+            redirect("/admin/dashboard"); // Arahkan admin ke dashboard admin
         } else if (userRole === "cashier") {
-            wauthparam.redirect = "/cashier/dashboard";
-        } else if (["user", "dosen"].includes(userRole)) {
-            wauthparam.redirect = "/menu";
+            redirect("/cashier/dashboard"); // Arahkan cashier ke dashboard cashier
         } else {
             setInner("content", "Role tidak dikenali.");
-            wauthparam.redirect = "/";
+            redirect("/");
         }
-        qrController(wauthparam);
     }
+    console.log(result);
 }
 
 wauthparam.auth_ws = "d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgzODUyODQxMDMxP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime = 18;
+wauthparam.redirect = "/menu";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
